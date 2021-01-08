53c54261be7b5d691bc608216c87423c80936d14
Juraji <github@juraji.nl>
2021-01-08 17:26:41 +0100

    Neo4jDtoMapper.kt: Added documentation to public functions; Added method to convert data classes to property maps

--- src/main/kotlin/nl/juraji/albums/util/Neo4jDtoMapper.kt
+++ src/main/kotlin/nl/juraji/albums/util/Neo4jDtoMapper.kt
@@ -7,12 +7,27 @@ import java.time.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 import kotlin.reflect.KType
+import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
+/**
+ * Utility to use data classes with Neo4j.
+ *
+ * @throws MissingPrimaryConstructorException When [T] does not have a primary constructor.
+ */
 class Neo4jDtoMapper<T : Any>(dtoClass: KClass<T>) {
+    private val memberProperties = dtoClass.memberProperties
     private val primaryConstructor = dtoClass.primaryConstructor
         ?: throw MissingPrimaryConstructorException(dtoClass)
 
+    /**
+     * Map a [Record] to [T].
+     *
+     * @param record The [Record] to read from.
+     * @param nodeLabel Optional node label, used in the Cypher query, to identify this node. Defaults to "n".
+     * @throws RequiredPropertyNotFoundException When a property, which is required in [T], can not be found in the given [Record].
+     * @throws CanNotMapValueException When a value can not be mapped, due to an unsupported type in [T].
+     */
     fun recordToDto(record: Record, nodeLabel: String = "n"): T {
         val node = record.get(nodeLabel).asNode()
 
@@ -23,10 +38,26 @@ class Neo4jDtoMapper<T : Any>(dtoClass: KClass<T>) {
         return primaryConstructor.callBy(parameterValues)
     }
 
+    /**
+     * Create a [Map] of the given instance of [T].
+     * Generally used as properties map in CREATE or MERGE Cypher queries.
+     *
+     * NULL values are omitted from the output map.
+     *
+     * @param dto An instance of [T].
+     */
+    fun dtoToPropertiesMap(dto: T): Map<String, Any> = memberProperties
+        .mapNotNull { prop ->
+            val value = prop.get(dto)
+            if (value != null) prop.name to value
+            else null
+        }
+        .toMap()
+
     private fun findNodeValueForParameter(node: Node, parameter: KParameter): Pair<KParameter, *> = when {
         node.containsKey(parameter.name) -> parameter to getNodeValue(node.get(parameter.name), parameter.type)
         parameter.type.isMarkedNullable -> parameter to null
-        else -> throw RequiredParameterNotFoundException(parameter, node)
+        else -> throw RequiredPropertyNotFoundException(parameter, node)
     }
 
     private fun getNodeValue(value: Value, type: KType): Any = when (type.classifier) {
@@ -53,7 +84,7 @@ class Neo4jDtoMapper<T : Any>(dtoClass: KClass<T>) {
     class MissingPrimaryConstructorException(dtoClass: KClass<*>) :
         DtoMappingException("No primary constructor for ${dtoClass.qualifiedName}!")
 
-    class RequiredParameterNotFoundException(parameter: KParameter, node: Node) :
+    class RequiredPropertyNotFoundException(parameter: KParameter, node: Node) :
         DtoMappingException("Parameter ${parameter.name} is required, but not found in Node $node")
 
     class CanNotMapValueException(value: Value, type: KType) :
