3290c0512d9db3bee02cf5e7c39510d3a037fc62
Juraji <github@juraji.nl>
2021-03-25 20:35:07 +0100

    Create add tag to picture in repository

---
+++ src/main/kotlin/nl/juraji/albums/domain/pictures/PictureTagsRepository.kt
@@ -0,0 +1,29 @@
+package nl.juraji.albums.domain.pictures
+
+import nl.juraji.albums.domain.tags.Tag
+import org.springframework.data.neo4j.repository.ReactiveNeo4jRepository
+import org.springframework.data.neo4j.repository.query.Query
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
+interface PictureTagsRepository : ReactiveNeo4jRepository<Tag, String> {
+
+    @Query(
+        """
+        MATCH (:Picture {id: $ pictureId})-[:HAS_TAG]->(t:Tag)
+        RETURN t ORDER BY t.label
+    """
+    )
+    fun findPictureTags(pictureId: String): Flux<Tag>
+
+    @Query(
+        """
+        MATCH (p:Picture {id: $ pictureId})
+        MATCH (t:Tag {id: $ tagId})
+        
+        MERGE (p)-[:HAS_TAG]->(t)
+        RETURN t
+    """
+    )
+    fun addTagToPicture(pictureId: String, tagId: String): Mono<Tag>
+}
---
+++ src/test/kotlin/nl/juraji/albums/domain/pictures/PictureTagsRepositoryTest.kt
@@ -0,0 +1,90 @@
+package nl.juraji.albums.domain.pictures
+
+import nl.juraji.albums.util.BaseTestHarnessConfig
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.neo4j.DataNeo4jTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.data.neo4j.core.Neo4jClient
+import reactor.test.StepVerifier
+
+@DataNeo4jTest
+class PictureTagsRepositoryTest {
+
+    @Autowired
+    private lateinit var pictureTagsRepository: PictureTagsRepository
+
+    @Autowired
+    private lateinit var neo4jClient: Neo4jClient
+
+    @Test
+    fun `should find picture tags`() {
+        val result = pictureTagsRepository.findPictureTags("picture1")
+
+        StepVerifier.create(result)
+            .expectNextMatches { it.id == "tag1" }
+            .expectNextMatches { it.id == "tag2" }
+            .expectNextMatches { it.id == "tag3" }
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should add tag to picture`() {
+        val returnValue = pictureTagsRepository.addTagToPicture("picture1", "tag3")
+
+        StepVerifier.create(returnValue)
+            .expectNextMatches { it.id == "tag3" }
+            .verifyComplete()
+
+        val result = neo4jClient.query(
+            """
+            MATCH (:Picture {id: 'picture1'})-[:HAS_TAG]->(t:Tag {id: 'tag3'})
+            RETURN t
+        """
+        )
+            .fetch()
+            .all()
+
+        assertEquals(1, result.size)
+    }
+
+    @TestConfiguration
+    class TestHarnessConfig : BaseTestHarnessConfig() {
+        // language=Cypher
+        override fun withFixtureQuery(): String = """
+            CREATE (picture:Picture {
+                id: 'picture1',
+                name: 'Picture 1',
+                type: 'UNKNOWN',
+                width: 0,
+                height: 0,
+                fileSize: 0,
+                addedOn: localdatetime('2021-03-25T19:49:00'),
+                lastModified: localdatetime('2021-03-25T19:49:00')
+            })
+            
+            CREATE (tag1:Tag {
+                id: 'tag1',
+                label: 'Tag 1',
+                textColor: '#000000',
+                color: '#ffffff'
+            })<-[:HAS_TAG]-(picture)
+            
+            CREATE (tag2:Tag {
+                id: 'tag2',
+                label: 'Tag 2',
+                textColor: '#000000',
+                color: '#ffffff'
+            })<-[:HAS_TAG]-(picture)
+            
+            CREATE (tag3:Tag {
+                id: 'tag3',
+                label: 'Tag 3',
+                textColor: '#000000',
+                color: '#ffffff'
+            })
+        """.trimIndent()
+
+    }
+}
