4c49ce6a19944b225a209e31856d67d4b91fcc5b
Juraji <github@juraji.nl>
2021-04-29 01:13:34 +0200

    Set frontend to have a max concurrency of 5 picture uploads; More k8s resource tweaks; Added init container to backend deployment, making it wait for neo4j to become available

--- src/web/environments/environment.prod.ts
+++ src/web/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiBaseUri: `${window.origin}/api`,
   defaultPageSize: 50,
-  maxConcurrentUpload: 8,
+  maxConcurrentUpload: 5,
   pageSizeOptions: [50, 100, 200, 500],
 };
